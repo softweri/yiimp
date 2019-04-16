ad95151101ec98f5f52be7735c3cb59affe91482
softweri <softweri@gmail.com>
2019-04-16 17:40:57 +0300

    Update client.h

--- stratum/client.h
+++ stratum/client.h
@@ -31,8 +31,8 @@ struct YAAMP_CLIENT_ALGO
 
 #define YAAMP_JOB_MAXHISTORY	16
 
-#define MIN_ADDRESS_LEN 30 /* BTC len can be as few as 26 chars, but gen. 33 or 34 */
-#define MAX_ADDRESS_LEN 35 /* DCR */
+#define MIN_ADDRESS_LEN 1 /* BTC len can be as few as 26 chars, but gen. 33 or 34 */
+#define MAX_ADDRESS_LEN 25 /* DCR */
 
 class YAAMP_CLIENT: public YAAMP_OBJECT
 {
