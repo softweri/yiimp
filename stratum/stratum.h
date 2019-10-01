3d5732087403bc96f86375dc3f1fe692badf4819
softweri <softweri@gmail.com>
2019-10-01 17:15:29 +0300

    Update stratum.h

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -150,7 +150,7 @@ void scrypt_N_R_1_256(const char* input, char* output, uint32_t N, uint32_t R, u
 void sha256_hash_hex(const char *input, char *output, unsigned int len);
 void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 
-	
+#include "algos/equi.h"	
 #include "algos/a5a.h"
 #include "algos/aergo.h"
 #include "algos/allium.h"
